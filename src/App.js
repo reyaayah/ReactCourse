fdd997d2e305dbc7a6b6876bca29c7ac4da5966f
Riya Awal <reeyaawal123@gmail.com>
2024-10-21 22:58:28 +0545

    Building a form & handling Submission

--- src/App.js
+++ src/App.js
@@ -25,9 +25,21 @@ function Logo() {
   return <h1 className="logo">✈️🚢TimeTamer🌟👜</h1>
 }
 function Form() {
-  return <div className="add-form">
-    What do you need for your trip?
-  </div>
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h1>What do you need for your trip🥰?</h1>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>{num}</option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button className="add">Add</button>
+    </form>
+  )
 }
 
 function PackingList() {
