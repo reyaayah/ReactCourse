7d2d6cc554c84fa25732de6af271b800ab28d9fd
Riya Awal <reeyaawal123@gmail.com>
2024-10-21 17:19:47 +0545

    Building the layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-  );
+  )
 }
-
-export default App;
+function Logo() {
+  return <h1 className="logo">✈️🚢TimeTamer🌟👜</h1>
+}
+function Form() {
+  return <div className="add-form">
+    What do you need for your trip?
+  </div>
+}
+function PackingList() {
+  return <div className="list">
+    <h1>LIST</h1>
+  </div>
+}
+function Stats() {
+  return (
+    <footer className="stats">You have X items in the list, and You already packed X items</footer>
+  )
+}
